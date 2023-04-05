75d4699aafa9eab12ac7cbbb0de4efbbb087f3e5
CptGrave <lukastrevo@gmail.com>
2023-04-05 11:53:20 +0200

    add showmore npm

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,11 @@
+import ShowMoreText from "react-show-more-text";
 export default function Book(props) {
   return(
     <div>
       <h2>{props.title}</h2>
       <img src={props.img}></img>
-      <p>{props.snippet}</p>
+      <h3>Book description</h3>
+      <ShowMoreText>{props.snippet}</ShowMoreText>
       <p>My rate in stars</p>
       <p>Date of reading</p>
     </div>
